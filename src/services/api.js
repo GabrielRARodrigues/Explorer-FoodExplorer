fb34b6689aaa501264e54c1b0e618a2ca05fc042
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-01-07 21:44:55 -0300

    Version 0.1.1

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://foodexplorer-api-6hfx.onrender.com/',
+  baseURL: 'https://explorer-api-gabrielrodrigues.koyeb.app/',
   withCredentials: true
 })
